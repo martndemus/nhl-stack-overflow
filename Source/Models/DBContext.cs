4e19fabbaa5cb9ef630ecd2e173b089fb20de684
Marten Schilstra <martenschilstra@gmail.com>
2012-02-16 13:48:10 +0100

    added missing stuff

---
+++ Source/Models/DBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace NHLStackOverflow.Models
+{
+    public class NHLSODBContext : DbContext
+    {
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<Badge> Badges { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Read> Read { get; set; }
+        public DbSet<TagRegel> TagRegels { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
