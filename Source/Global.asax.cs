429bb424d0c011c609f4cba9cef112b219003591
Marten Schilstra <martenschilstra@gmail.com>
2012-02-16 10:07:25 +0100

    added models

--- Source/Global.asax.cs
+++ Source/Global.asax.cs
@@ -22,7 +22,7 @@ namespace NHLStackOverflow
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                "Default", // Route name
+                "Index", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
---
+++ Source/Models/CommentModel.cs
@@ -0,0 +1,21 @@
+﻿using System
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace NHLStackOverflow.Models
+{mbox;
+    public class Comment
+    {m
+        [Required]
+        [Key] // <- Unique key!
+        public int Id { get; set; }
+
+        public Question Question { get; set; }
+        public Answer Answer { get; set; }
+        public User User { get; set; }
+        public string Content { get; set; }
+        public string Created { get; set; }
+        public string
+
+    }
+}
