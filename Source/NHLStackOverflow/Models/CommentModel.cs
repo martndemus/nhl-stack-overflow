9d0dbe7d4f08c183f261c07d70fa3af8d29efd42
Eddie Sprietsma <eddiesprietsma@hotmail.com>
2012-02-22 11:21:25 +0100

    Models validation toegevoegd

--- Source/NHLStackOverflow/Models/CommentModel.cs
+++ Source/NHLStackOverflow/Models/CommentModel.cs
@@ -6,25 +6,38 @@ namespace NHLStackOverflow.Models
 {
     public class Comment : IValidatableObject
     {
+        public Comment()
+        {
+            this.Votes = 0;
+            this.Created_At = DateTime.Now.ToString();
+        }
         // GUID
+        [Required]
         public int CommentID { get; set; }
 
         // Data
+        [Required]
+        [MinLength(50)]
         public string Content { get; set; }
         public int Votes { get; set; }
 
         // TimeStamps
+        [Required]
         public string Created_At { get; set; }
         public string LastEdited { get; set; }
 
         // Relations
+        [Required]
         public User User { get; set; }
         public Question Question { get; set; }
         public Answer Answer { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            throw new NotImplementedException();
+            if (Question != null && Answer != null || Question == null && Answer == null)
+            {
+                yield return new ValidationResult("");
+            }
         }
     }
 }
--- Source/NHLStackOverflow/Models/FavoriteModel.cs
+++ Source/NHLStackOverflow/Models/FavoriteModel.cs
@@ -6,19 +6,22 @@ namespace NHLStackOverflow.Models
 {
     public class Favorite : IValidatableObject
     {
+        public Favorite()
+        {
+            this.Created_At = DateTime.Now.ToString();
+        }
         // GUID
+        [Required]
         public int FavoriteID { get; set; }
 
         // TimeStamp
+        [Required]
         public string Created_At { get; set; }
 
         // Relations
+        [Required]
         public User User { get; set; }
+        [Required]
         public Question Post { get; set; }
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
