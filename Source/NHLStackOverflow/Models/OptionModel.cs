d29f4b0910117476a7859a6e5203e402b6e45a58
Marten Schilstra <martenschilstra@gmail.com>
2012-02-22 11:14:21 +0100

    Added option model

---
+++ Source/NHLStackOverflow/Models/OptionModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NHLStackOverflow.Models
+{
+    public class OptionModel
+    {
+        // GUID
+        public int OptionID { get; set; }
+
+        // Data
+        [Required]
+        [Key]
+        public string Name { get; set; }
+
+        [Required]
+        public string Value { get; set; }
+    }
+}
