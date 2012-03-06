dae6aa668a9e9eab40f06407adebfbb2c4cf5dfd
Mathijs Jonker <mathijsjonker@msn.com>
2012-03-06 13:59:58 +0100

    Log in en uit werkend

---
+++ Source/NHLStackOverflow/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using NHLStackOverflow.Models;
+using System.Linq;
+using System;
+
+namespace NHLStackOverflow.Controllers
+{
+    public class AccountController : Controller
+    {
+        private NHLdb db = new NHLdb();
+        //
+        // GET: /Account/LogIn
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/LogIn
+
+        [HttpPost]
+        public ActionResult LogIn(LogOnModel user, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                // fields are filled in correctly
+                var userPass = from memberUser in db.Users
+                               where memberUser.UserName == user.UserName
+                               select memberUser.Password;
+                if (userPass.Count() == 1 && user.Password == userPass.First())
+                {
+                    // submittings were correct.
+                    FormsAuthentication.SetAuthCookie(user.UserName, user.stayLoggenIn);
+                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The username or password didn't match. Please try again.");
+                }
+            }
+
+            return View(user);
+        }
+
+        //
+        // GET: /Account/LogOff
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
--- Source/NHLStackOverflow/Models/ReadModel.cs
+++ Source/NHLStackOverflow/Models/ReadModel.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace NHLStackOverflow.Models
 {
     public class Read
