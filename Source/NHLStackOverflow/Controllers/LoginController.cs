bb7fc41fad9a290153872d9455c6707b6218e131
Marten Schilstra <martenschilstra@gmail.com>
2012-03-07 10:58:43 +0100

    changed login controller + lostpassword

---
+++ Source/NHLStackOverflow/Controllers/LoginController.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+using NHLStackOverflow.Classes;
+using NHLStackOverflow.FormDataModels;
+using NHLStackOverflow.Models;
+
+namespace NHLStackOverflow.Controllers
+{
+    public class LoginController : Controller
+    {
+        private NHLdb db = new NHLdb();
+
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Login/
+
+        [HttpPost]
+        public ActionResult Index(Login user, string returnUrl)
+        {
+            // Check if both Username and Password are present
+            if (user.Password != null && user.UserName != null)
+            {
+                // Find a user with the given username in the database
+                var userPass = from memberUser in db.Users
+                               where memberUser.UserName == user.UserName
+                               select memberUser.Password;
+
+                // If the user exists check if the password is correct
+                if (userPass.Count() == 1 && PasswordHasher.Hash(user.Password) == userPass.First())
+                {
+                    // User is logged in, set a cookie
+                    FormsAuthentication.SetAuthCookie(user.UserName, true);
+
+                    // Go to home page
+                    return RedirectToAction("index", "home");
+                }
+                else
+                {
+                    // Display an error
+                    ModelState.AddModelError("", "Ongeldig gebruikersnaam of wachtwoord.");
+                }
+            }
+
+            ViewBag.UserName = user.UserName;
+
+            return View();
+        }
+
+        public ActionResult WachtwoordKwijt()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult WachtwoordKwijt()
+        {
+            // Todo wachtwoordkwijt mailer maken
+
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("index", "home");
+        }
+    }
+}
