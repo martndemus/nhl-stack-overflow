1f531aeeafc3241d40bc9493ba5f3ebad094e13c
Eddie Sprietsma <eddiesprietsma@hotmail.com>
2012-03-20 13:10:57 +0100

    Tagpagina bijgewerkt

--- Source/NHLStackOverflow/Controllers/TagsController.cs
+++ Source/NHLStackOverflow/Controllers/TagsController.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using NHLStackOverflow.Classes;
 using NHLStackOverflow.Models;
+using System.Collections.Generic;
 
 namespace NHLStackOverflow.Controllers
 {
@@ -36,11 +37,40 @@ namespace NHLStackOverflow.Controllers
         // GET: /Tags/search/int TagID
         public ActionResult Search(int id)
         {
+            HTMLSanitizer hs = new HTMLSanitizer();
+
             var TagsSearched = from questionView in db.Questions
                                join c in db.QuestionTags on questionView.QuestionID equals c.QuestionId
                                where c.TagId == id
                                select questionView;
             ViewBag.TagSearch = TagsSearched;
+
+            var TagsNames = from tagsName in db.Tags
+                            where tagsName.TagID == id
+                            select tagsName;
+            ViewBag.TagNames = TagsNames.First();
+
+            List<TagsIDs> abc = new List<TagsIDs>();
+
+            //tags in question
+
+            foreach (Question t in TagsSearched)
+            {
+                var TagList = from tagsQuestion in db.Tags
+                              join c in db.QuestionTags on tagsQuestion.TagID equals c.TagId
+                              where c.QuestionId == t.QuestionID
+                              select tagsQuestion;
+
+                foreach (Tag i in TagList)
+                {
+                    abc.Add(new TagsIDs(i, t.QuestionID));
+
+                    // Sanitize each tag its name
+                    i.Name = hs.Sanitize(i.Name, HTMLSanitizerOption.StripTags);
+                }
+            }
+            ViewBag.Helper = abc;
+
             return View();
         }
 
