2e98de8cbd1ab67022624161510664798a1f24d8
Mathijs Jonker <mathijsjonker@msn.com>
2012-03-26 12:33:17 +0200

    Mailer

--- Source/NHLStackOverflow/Controllers/WidgetController.cs
+++ Source/NHLStackOverflow/Controllers/WidgetController.cs
@@ -29,10 +29,16 @@ namespace NHLStackOverflow.Controllers
                               where b.UserId == User.UserID
                               select b).Count();
 
+                // get the amount of unread messages
+                var MessageCount = (from m in db.Messages
+                                    where m.Viewed == 0 && m.ReceiverId == User.UserID
+                                    select m).Count();
+
                 // Add user info to viewbag
                 ViewBag.User = User;
                 ViewBag.Usermeta = Usermeta;
                 ViewBag.BadgeCount = BadgeCount;
+                ViewBag.UnreadMessages = MessageCount;
 
                 // Gravater url for user Avater
                 ViewBag.GravatarURL = String.Format("http://www.gravatar.com/avatar/{0}?s=86&d=mm&r=g", Cryptography.GravatarHash(User.Email));
