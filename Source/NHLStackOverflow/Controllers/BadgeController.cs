8f7b9cb309763a247334c60d73908cc9f55d73f9
Eddie Sprietsma <eddiesprietsma@hotmail.com>
2012-03-29 09:16:19 +0200

    Badges strikes back

--- Source/NHLStackOverflow/Controllers/BadgeController.cs
+++ Source/NHLStackOverflow/Controllers/BadgeController.cs
@@ -3,12 +3,12 @@ using System.Linq;
 using System.Web.Mvc;
 using NHLStackOverflow.Models;
 using NHLStackOverflow.Classes;
+using NHLStackOverflow.Models.FormDataModels;
 
 namespace NHLStackOverflow.Controllers
 {
     public class BadgeController : Controller
     {
-
         private NHLdb db = new NHLdb();
         //
         // GET: /Badge/
@@ -19,7 +19,22 @@ namespace NHLStackOverflow.Controllers
             var badgeList = from badges in db.Badges
                             group badges by badges.Name into a
                             select new BadgeCount() { badge = a.FirstOrDefault(), count = a.Count() };
+
             ViewBag.badgesList = badgeList;
+
+            var badgeDesList = new List<BadgeDe>{
+                new BadgeDe(1, "Stel je eerste vraag", "Bronze"),
+                new BadgeDe(2, "Geef je eerste antwoord", "Bronze"),
+                new BadgeDe(3, "Maak je eerste Tag aan", "Bronze"),
+                new BadgeDe(4, "Maak je 20e Tag aan", "Zilver"),
+                new BadgeDe(5, "Maak je 9000e Tag aan", "Platina"),
+                new BadgeDe(6, "Geef je 20e antwoord", "Zilver"),
+                new BadgeDe(7, "Geef je 100e antwoord", "Goud"),
+                new BadgeDe(8, "Stel je 20e vraag", "Zilver"),
+                new BadgeDe(9, "Stel je 100e vraag", "Goud")
+            };
+
+            ViewBag.BadgeDescription = badgeDesList;
 
             return View();
         }
@@ -43,6 +58,23 @@ namespace NHLStackOverflow.Controllers
                 test.Add(badgeUsers.First());
             }
             ViewBag.badgesUsers = test;
+
+            var badgeDesList = new List<BadgeDe>{
+                new BadgeDe(1, "Stel je eerste vraag", "Bronze"),
+                new BadgeDe(2, "Geef je eerste antwoord", "Bronze"),
+                new BadgeDe(3, "Maak je eerste Tag aan", "Bronze"),
+                new BadgeDe(4, "Maak je 20e Tag aan", "Zilver"),
+                new BadgeDe(5, "Maak je 9000e Tag aan", "Platina"),
+                new BadgeDe(6, "Geef je 20e antwoord", "Zilver"),
+                new BadgeDe(7, "Geef je 100e antwoord", "Goud"),
+                new BadgeDe(8, "Stel je 20e vraag", "Zilver"),
+                new BadgeDe(9, "Stel je 100e vraag", "Goud")
+            };
+            int idtje = badgeList.First().BadgeID;
+            ViewBag.BadgeDescription = (from badgeDescript in badgeDesList
+                                        where badgeDescript.BadgeID == idtje
+                                        select badgeDescript.Description).First();
+
             return View();
         }
 
---
+++ Source/NHLStackOverflow/Models/FormDataModels/Badge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NHLStackOverflow.Models.FormDataModels
+{
+    public class BadgeDe
+    {
+        public BadgeDe(int badgeID, string description, string color)
+        {
+            this.BadgeID = badgeID;
+            this.Description = description;
+            this.Color = color;
+        }
+        public int BadgeID { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+    }
+}
